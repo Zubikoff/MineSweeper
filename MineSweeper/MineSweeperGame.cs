85241c99d3ac5f9b320e8e709183ceff944f1052
Pavel Zubaryk <pavelzubar@gmail.com>
2020-09-09 18:35:28 +0300

    Added demining method and method opening the field.

--- MineSweeper/MineSweeperGame.cs
+++ MineSweeper/MineSweeperGame.cs
@@ -13,7 +13,7 @@ namespace MineSweeper
         /// </summary>
         protected bool[,] MineField;
 
-        public int[,] Field { get; private set; }
+        public int?[,] Field { get; private set; }
 
         private bool MinesSet;
 
@@ -26,8 +26,10 @@ namespace MineSweeper
         public MineSweeperGame(int fieldW, int fieldH, int mines)
         {
             MineField = new bool[fieldW, fieldH];
+            Field = new int?[fieldW, fieldH];
             Mines = mines;
             MinesSet = false;
+
         }
 
         public static MineSweeperGame StartNew(int fieldW, int fieldH, int mines)
@@ -41,17 +43,17 @@ namespace MineSweeper
         /// </summary>
         /// <param name="clickedRow"></param>
         /// <param name="clickedCol"></param>
-        protected internal void SetMines(int clickedRow, int clickedCol)
+        protected internal void SetMines(int clickedCol, int clickedRow)
         {
-            MineField[clickedRow, clickedCol] = true; // temporarily set user's move as "mine"
+            MineField[clickedCol, clickedRow] = true; // temporarily set user's move as "mine"
 
             SetMines();
 
-            MineField[clickedRow, clickedCol] = false; // clear user's move from a mine
+            MineField[clickedCol, clickedRow] = false; // clear user's move from a mine
         }
 
         /// <summary>
-        /// Randomly spitting mines across the field.
+        /// Randomly spitting mines across the field. This method ignores user's move.
         /// </summary>
         protected internal void SetMines()
         {
@@ -72,6 +74,82 @@ namespace MineSweeper
                 }
             }
             MinesSet = true;
+        }
+
+        /// <summary>
+        /// Making user's move.
+        /// </summary>
+        /// <returns>Returns "true" if demining successful. Returns "false" if you picked a mine</returns>
+        public bool DemineCell(int clickedCol, int clickedRow)
+        {
+            if (clickedRow < 0 || clickedCol < 0 || clickedRow >= FieldHeight || clickedCol >= FieldWidth)
+                throw new IndexOutOfRangeException("Переданные координаты находятся за пределами игрового поля.");
+
+            if (MineField[clickedCol, clickedRow])
+                return false;
+
+            FieldOpen(clickedCol, clickedRow);
+
+            return true;
+        }
+
+        /// <summary>
+        /// Open the selected cell
+        /// </summary>
+        protected void FieldOpen(int clickedCol, int clickedRow)
+        {
+            // if indexes are out of range we simply return
+            if (clickedRow < 0 || clickedCol < 0 || clickedRow >= FieldHeight || clickedCol >= FieldWidth)
+                return;
+
+            // if there is a mine in a cell or this cell is already processed we return
+            if (MineField[clickedCol, clickedRow] || Field[clickedCol, clickedRow] != null) 
+                return;
+            else
+            {
+                int number = 0;
+
+                // counting the surrounding mines
+                if (IfTheMine(clickedCol - 1, clickedRow - 1)) number++;
+                if (IfTheMine(clickedCol    , clickedRow - 1)) number++;
+                if (IfTheMine(clickedCol + 1, clickedRow - 1)) number++;
+
+                if (IfTheMine(clickedCol - 1, clickedRow    )) number++;
+                if (IfTheMine(clickedCol + 1, clickedRow    )) number++;
+
+                if (IfTheMine(clickedCol - 1, clickedRow + 1)) number++;
+                if (IfTheMine(clickedCol    , clickedRow + 1)) number++;
+                if (IfTheMine(clickedCol + 1, clickedRow + 1)) number++;
+
+                if (number == 0)
+                {
+                    // if there is no mines in surrounding cells we open all cells around us (recursive);
+                    FieldOpen(clickedCol - 1, clickedRow - 1);
+                    FieldOpen(clickedCol, clickedRow - 1);
+                    FieldOpen(clickedCol + 1, clickedRow - 1);
+
+                    FieldOpen(clickedCol - 1, clickedRow);
+                    FieldOpen(clickedCol + 1, clickedRow);
+
+                    FieldOpen(clickedCol - 1, clickedRow + 1);
+                    FieldOpen(clickedCol, clickedRow + 1);
+                    FieldOpen(clickedCol + 1, clickedRow + 1);
+                }
+
+                Field[clickedCol, clickedRow] = number;
+            }
+        }
+
+        /// <summary>
+        /// Checking the mine in a current cell. Ignores going out of range.
+        /// </summary>
+        /// <returns>Returns "true" if there is a mine in a cell. Returns "false" in all other ways</returns>
+        protected bool IfTheMine(int clickedCol, int clickedRow)
+        {
+            if (clickedRow < 0 || clickedCol < 0 || clickedRow >= FieldHeight || clickedCol >= FieldWidth)
+                return false;
+            else 
+                return MineField[clickedCol, clickedRow];
         }
     }
 }
