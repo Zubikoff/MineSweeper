bdadeff49392769d6dd51ebfe9031d122623e4f5
Pavel Zubaryk <pavelzubar@gmail.com>
2020-09-09 14:08:49 +0300

    Add project files.

---
+++ MineSweeper/MineSweeperGame.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MineSweeper
+{
+    public class MineSweeperGame
+    {
+        protected int[,] MineField;
+
+        public int FieldWidth { get; private set; }
+
+        public int FieldHeight { get; private set; }
+
+        public int Mines { get; private set; }
+
+        public MineSweeperGame(int fieldW, int fieldH, int mines)
+        {
+            MineField = new int[fieldW, fieldH];
+            Mines = mines;
+        }
+
+        public static MineSweeperGame StartNew(int fieldW, int fieldH, int mines)
+        {
+            return new MineSweeperGame(fieldW, fieldH, mines);
+        }
+
+        protected internal void SetMines()
+        {
+
+        }
+    }
+}
